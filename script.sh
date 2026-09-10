1c4b2ae76143e983a5eb08134dbe7f764b1d825e
Vladislav Blokhin <v.blohin@d-services.ru>
2026-09-10 17:20:40 +0300

    feat: добавлен цикл мониторинга, uptime и запись в файл

--- script.sh
+++ script.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 INTERVAL=5
-echo "--- $(date '+%Y-%m-%d %H:%M:%S') ---"
-free -h
-df -h
+LOG_FILE="monitor.log"
+
+echo "Мониторинг запущен. Интервал: ${INTERVAL} сек. Вывод пишется в ${LOG_FILE}"
+
+while true; do
+    {
+        echo "--- $(date '+%Y-%m-%d %H:%M:%S') ---"
+        free -h
+        echo ""
+        df -h
+        echo ""
+        uptime
+        echo ""
+    } >> "$LOG_FILE"
+    sleep "$INTERVAL"
+done
